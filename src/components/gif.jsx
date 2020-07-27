74e190da4ce8047e17f7afb19619bd4b485d68fd
Laurent Treuil <laurent.treuil@gmail.com>
2020-07-27 19:17:37 +0200

    componnents implémentation in the app

---
+++ src/components/gif.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render() {
+    return (
+      <img src="https://media0.giphy.com/media/cEYFeDKVPTmRgIG9fmo/giphy.gif" alt="gif"/>
+    );
+  }
+}
+
+export default Gif;
